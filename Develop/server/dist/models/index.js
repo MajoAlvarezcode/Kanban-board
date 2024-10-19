cf428c2c14b55dae151ee4ca96f6feaab574b896
Majo Alvarez <majo.bootcamp@gmail.com>
2024-10-19 13:56:12 -0600

    change in port

--- Develop/server/dist/models/index.js
+++ Develop/server/dist/models/index.js
@@ -3,15 +3,20 @@ dotenv.config();
 import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
+// Asegúrate de que las variables de entorno están definidas
+const dbName = process.env.DB_NAME;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined; // Convierte a número
 const sequelize = process.env.DB_URL
     ? new Sequelize(process.env.DB_URL)
-    : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+    : new Sequelize(dbName, dbUsername, dbPassword, {
         host: 'localhost',
         dialect: 'postgres',
-        port: 5433,
+        ...(dbPort !== undefined && { port: dbPort }), // Agrega la propiedad port solo si está definida
         dialectOptions: {
-            decimalNumbers: true,
-        },
+            decimalNumbers: true
+        }
     });
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
--- Develop/server/src/models/index.ts
+++ Develop/server/src/models/index.ts
@@ -5,21 +5,27 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+// Asegúrate de que las variables de entorno están definidas
+const dbName = process.env.DB_NAME!;
+const dbUsername = process.env.DB_USERNAME!;
+const dbPassword = process.env.DB_PASSWORD!;
+const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined; // Convierte a número
+
+const sequelize = process.env.DB_URL 
+  ? new Sequelize(process.env.DB_URL) 
+  : new Sequelize(dbName, dbUsername, dbPassword, {
       host: 'localhost',
       dialect: 'postgres',
-      port: 5433,
+      ...(dbPort !== undefined && { port: dbPort }), // Agrega la propiedad port solo si está definida
       dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+          decimalNumbers: true
+      }
+  });
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser' });
 
 export { sequelize, User, Ticket };

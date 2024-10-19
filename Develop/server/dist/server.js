58da428b3670a0688aad1e1148edba61b03e4b01
Majo Alvarez <majo.bootcamp@gmail.com>
2024-10-19 12:48:42 -0600

    added production

--- Develop/server/dist/server.js
+++ Develop/server/dist/server.js
@@ -3,15 +3,29 @@ dotenv.config();
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3001;
-const forceDatabaseRefresh = false;
 // Serves static files in the entire client's dist folder
 app.use(express.static(('../client/dist')));
 app.use(express.json());
-app.use(routes);
-sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is listening on port ${PORT}`);
+app.use('/', routes);
+if (process.env.NODE_ENV === 'production') {
+    app.get('*', (_req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
+}
+;
+sequelize.sync()
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is live at http://localhost:${PORT}`);
+    });
+})
+    .catch((err) => {
+    console.error('Failed to sync the database:', err);
 });
